5dd3b9033e53a531370cd52e0076542cb6ec4739
artioki <kmm.19@uni-dubna.ru>
2022-03-18 16:57:44 +0300

    delete firstload let

--- public/src/index.js
+++ public/src/index.js
@@ -1,32 +1,27 @@
-const  hashchanged  = async () =>{
-  console.log("hashchange:"+window.location.pathname)
-  path=window.location.pathname
+const hashchanged = async () => {
+  console.log("hashchange:" + window.location.pathname);
+  path = window.location.pathname;
 
-  url = window.location.origin+'/api'+path
-  console.log(url)
+  url = window.location.origin + "/api" + path;
+  console.log(url);
 
   let res = await fetch(url);
 
-  if (res.ok) { // если HTTP-статус в диапазоне 200-299
+  if (res.ok) {
+    // если HTTP-статус в диапазоне 200-299
     // получаем тело ответа (см. про этот метод ниже)
     document.getElementById("app").innerHTML = await res.text();
   } else {
     alert("Ошибка HTTP: " + response.status);
   }
-} 
+};
 
-window.onpopstate= hashchanged;
-
-  let firstload = false;
-
-  if(!firstload){
-    console.log(window.location.pathname)
-    myFun(window.location.pathname)
-  }
-
-function myFun(path){
-
+function myFun(path) {
   history.pushState(null, null, path);
-  hashchanged()
+  hashchanged();
 }
-console.log("end")
+
+console.log("first-start");
+window.onpopstate = hashchanged;
+myFun(window.location.pathname);
+console.log("first-end");
